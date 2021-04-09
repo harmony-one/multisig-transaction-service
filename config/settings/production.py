b730702a1811b590abeb87e0dbcc0c036e9e50b9
unlocker <blueraptor1001@gmail.com>
2021-04-09 10:14:59 -0700

    Update the Allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['13.82.227.107'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['hmny.io'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -75,7 +75,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='Gnosis Safe Push Service <noreply@gnosis.pm>'
+    default='Harmony Safe Push Service <hello@harmony.one>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
